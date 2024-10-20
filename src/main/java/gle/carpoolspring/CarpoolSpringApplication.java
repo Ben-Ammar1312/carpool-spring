2b458d31cd5e323a4f2a4d4c2677a34fc7ae5a34
Ben-Ammar1312 <ben.ammar1312@gmail.com>
2024-10-20 12:58:32 +0100

    Initial project structure

---
+++ src/main/java/gle/carpoolspring/CarpoolSpringApplication.java
@@ -0,0 +1,13 @@
+package gle.carpoolspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gle/carpoolspring/CarpoolSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gle.carpoolspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
